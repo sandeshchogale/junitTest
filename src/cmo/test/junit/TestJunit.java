4796182061450249637a202d2a1b81c5e0599b0f
sandesh chogale <94666700+sandeshchogale@users.noreply.github.com>
2023-06-20 19:21:08 +0530

    Add files via upload

---
+++ src/cmo/test/junit/TestJunit.java
@@ -0,0 +1,16 @@
+package cmo.test.junit;
+
+import cmo.test.MessageUtil;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJunit {
+	String message = "Hello World";
+	MessageUtil messageUtil = new MessageUtil(message);
+
+	@Test
+	public void testPrintMessage() {
+		assertEquals(message, messageUtil.printMessage());
+	}
+}
